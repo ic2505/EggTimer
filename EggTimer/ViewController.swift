bd67746f9c618a245f3219249bddd5d8146d3de8
ilker <ilkerciliv@gmail.com>
2022-02-07 17:15:44 -0500

    timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,21 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var secondsRemaining = 60 // initialize with random value
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        let hardness = sender.currentTitle
+        let hardness = sender.currentTitle!
         
-        switch hardness {
-        case "Soft":
-            print(eggTimes["Soft"]!)
-        case "Medium":
-            print(eggTimes["Medium"]!)
-        case "Hard":
-            print(eggTimes["Hard"]!)
-        default:
-            print("Error")
+        secondsRemaining = eggTimes[hardness]! // reassign actual time
+        
+        // Timer init: updates every 1.0 seconds, repeats every second, selector needs a function name to call every second. To call this function we need @objc because its remnant from objective C Times. Will be clarified later.
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateCounter() {
+        //example functionality
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds")
+            secondsRemaining -= 1
         }
     }
+    
+
+    
+
+    
 }
+
+
